929518dae0c957e7696ee7630d3649670e15709c
Wulanm <222310317@smkn4-tsm.sch.id>
2025-01-03 17:01:05 +0700

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",  // Menambahkan Bootstrap CSS
+    "bootstrap-icons/font/bootstrap-icons.css" // Menambahkan Bootstrap Icons CSS
+  ],
+  compatibilityDate: "2024-12-11",
+});
